01f4862cee71518cbe2ec3ca387a796f4cc91424
Jackson B <barkerj3@myumanitoba.ca>
2021-03-31 15:37:04 -0500

    Create a3.py
    just in case we wanted to start with the sample code he linked to
    
    -removed little bits of code as per his request that we dont copy paste any implementation
    
    - removed a parameter to the class that doesn't exist without some other stuff from berkley

---
+++ a3.py
@@ -0,0 +1,54 @@
+
+
+
+class ValueIterationAgent():
+  """
+      * Please read learningAgents.py before reading this.*
+
+      A ValueIterationAgent takes a Markov decision process
+      (see mdp.py) on initialization and runs value iteration
+      for a given number of iterations using the supplied
+      discount factor.
+  """
+  def __init__(self, mdp, discount = 0.9, iterations = 100):
+    """
+      Your value iteration agent should take an mdp on
+      construction, run the indicated number of iterations
+      and then act according to the resulting policy.
+    
+      Some useful mdp methods you will use:
+          mdp.getStates()
+          mdp.getPossibleActions(state)
+          mdp.getTransitionStatesAndProbs(state, action)
+          mdp.getReward(state, action, nextState)
+    """
+     
+    "*** YOUR CODE HERE ***"
+    
+  def getValue(self, state):
+    """
+      Return the value of the state (computed in __init__).
+    """
+
+  def getQValue(self, state, action):
+    """
+      The q-value of the state action pair
+      (after the indicated number of value iteration
+      passes).  Note that value iteration does not
+      necessarily create this quantity and you may have
+      to derive it on the fly.
+    """
+    "*** YOUR CODE HERE ***"
+
+  def getPolicy(self, state):
+    """
+      The policy is the best action in the given state
+      according to the values computed by value iteration.
+      You may break ties any way you see fit.  Note that if
+      there are no legal actions, which is the case at the
+      terminal state, you should return None.
+    """
+    "*** YOUR CODE HERE ***"
+
+  def getAction(self, state):
+    "Returns the policy at the state (no exploration)."
