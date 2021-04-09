a8ec391d0234ba849055cb949884c57cb75a8c4a
Jackson B <barkerj3@myumanitoba.ca>
2021-04-09 14:04:29 -0500

    Update a3.py
    -changed to use the GUI
    -put the GUI code into a3.py
    -default values of 0 everywhere

--- a3.py
+++ a3.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+import tkinter as tk
 
 #global variables for now, we may do something else with them
 HORIZONTAL = 0
@@ -14,29 +15,23 @@ DISCOUNT = 0
 NOISE = 0
 TRANSITION_COST = 0
 
-def createGrid():
-  #grid = np.zeros(shape=(HORIZONTAL,VERTICAL))
+def createGrid(horizontal,vertical):
 
-  #string version just to see where everything is
-  grid = np.full((HORIZONTAL,VERTICAL),'*',dtype=str)
+  grid = []
+  for h in range(horizontal):
+    row = []
+    for v in range(vertical):
+      row.append([[0,'↑'],[0,'←'],[0,'↓'],[0,'→']])
+    grid.append(row)
 
-  for boulder in BOULDER:
-    print(f"Boulder: {boulder}")
-    grid[boulder[0]][boulder[1]] = 'B'
-
-  for term in TERMINAL:
-    print(f"Terminal: {term}")
-    grid[term[0]][term[1]] = 'T'
-  grid[ROBOTSTARTSTATE[0]][ROBOTSTARTSTATE[1]] = 'S'
-  print(f"grid shape: {grid.shape}")
-  print(grid)
+  #print(grid)
+  return grid
   
-
 def readInput(filename='gridConf.txt'):
 
   read = open(filename)
   for line in read:
-    print(f"Line: {line}")
+    #print(f"Line: {line}")
     parseLine(line)
     
 def parseLine(line):
@@ -92,7 +87,7 @@ def parseLine(line):
   else:
     print("Unknown Field")
 
-  print(tokens)
+  #print(tokens)
 
 def tests():
   print("---Global Variables---")
@@ -108,9 +103,8 @@ def tests():
   print(f"Noise: {NOISE}")
   print(f"TransitionCost: {TRANSITION_COST}")
 
-  createGrid()
 
-class ValueIterationAgent():
+class ValueIterationAgent:
   """
       * Please read learningAgents.py before reading this.*
 
@@ -119,7 +113,7 @@ class ValueIterationAgent():
       for a given number of iterations using the supplied
       discount factor.
   """
-  def __init__(self, mdp, discount = 0.9, iterations = 100):
+  def __init__(self,grid, mdp=None, discount = 0.9, iterations = 100):
     """
       Your value iteration agent should take an mdp on
       construction, run the indicated number of iterations
@@ -162,6 +156,134 @@ class ValueIterationAgent():
   def getAction(self, state):
     "Returns the policy at the state (no exploration)."
 
+def main():
 
-readInput('gridConf.txt')
-tests()
+    window = tk.Tk()
+    readInput('gridConf.txt')
+    grid = createGrid(HORIZONTAL,VERTICAL)
+    #tests()
+
+    terminal_states = TERMINAL
+    boulder_states = BOULDER
+    num_iterations = 10
+
+    draw_board(window, grid, [row[:-1] for row in terminal_states], boulder_states,
+               max_reward(terminal_states), max_punishment(terminal_states), num_iterations)
+
+    window.mainloop()
+
+
+def max_reward(terminal_states):
+    max_reward = float('-inf')
+
+    for state in terminal_states:
+        if state[2] > max_reward:
+            max_reward = state[2]
+
+    return max_reward
+
+
+def max_punishment(terminal_states):
+    max_punishment = float('inf')
+
+    for state in terminal_states:
+        if state[2] < max_punishment:
+            max_punishment = state[2]
+
+    return max_punishment
+
+
+def draw_board(window, grid, terminal, boulders, max_reward, max_punishment, iterations):
+
+    canvas_width = 1000  # Width of the window
+    canvas_height = 600  # Length of the window
+
+    edge_dist = 10  # Distance of the board to the edge of the window
+    bottom_space = 100  # Distance from the bottom of the board to the bottom of the window
+    small_rect_diff = 10  # For terminal states, distance from outside rectangle to inside rectangle
+
+    rows = len(grid)  # Number of rows in the grid
+    cols = len(grid[0])  # Number of columns in the grid
+
+    edge_dist_triangle = 5  # Distance from tip of the triangle to the edge of the rectangle
+    triangle_height = int(0.1 * min(((canvas_width - 2 * edge_dist) / cols), ((canvas_height - edge_dist - bottom_space) / rows)))  # Height of the triangles
+    triangle_width = 2 * triangle_height  # Width of the triangles
+
+    canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, background='black')  # Create a black background
+
+    for row in range(rows):  # Loop through the rows of the grid
+        for col in range(cols):  # Loop through the columns of the grid
+            if [row, col] not in boulders:  # If it's not a boulder state
+                x1 = edge_dist + col * ((canvas_width - 2 * edge_dist) / cols)  # Top left x coordinate of the rectangle
+                y1 = edge_dist + row * ((canvas_height - edge_dist - bottom_space) / rows)  # Top left y coordinate of the rectangle
+                x2 = x1 + ((canvas_width - 2 * edge_dist) / cols)  # Bottom right x coordinate of the rectangle
+                y2 = y1 + ((canvas_height - edge_dist - bottom_space) / rows)  # Bottom right y coordinate of the rectangle
+
+                best_move = get_best_move(grid[row][col])  # Get the index of the maximum q-value for this cell
+                best_value = grid[row][col][best_move][0]  # Get the best q-value for this cell
+                best_direction = grid[row][col][best_move][1]  # Get the best direction out of this cell
+
+                if best_value >= 0:  # Best value is positive, so draw the rectangle in green
+                    canvas.create_rectangle(x1, y1, x2, y2, outline='white',
+                                            fill='#%02x%02x%02x' % (0, int(200 * min(best_value / max_reward, max_reward)), 0))  # Draw the rectangle of this cell
+                else:  # Best value is negative, so draw the rectangle in red
+                    canvas.create_rectangle(x1, y1, x2, y2, outline='white',
+                                            fill='#%02x%02x%02x' % (int(200 * min(best_value / max_punishment, -1 * max_punishment)), 0, 0))  # Draw the rectangle of this cell
+
+                canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=str(round(best_value, 2)),
+                                   font=('TkDefaultFont', int(0.25 * ((canvas_width - 2 * edge_dist) / cols))), fill='white')  # Print the best value in the middle of the cell
+
+                if [row, col] in terminal:  # If this cell is a terminal state
+                    x1 = x1 + small_rect_diff
+                    y1 = y1 + small_rect_diff
+                    x2 = x2 - small_rect_diff
+                    y2 = y2 - small_rect_diff
+                    canvas.create_rectangle(x1, y1, x2, y2, outline='white')  # Draw a smaller rectangle inside
+                else:  # Not a terminal state, so draw an arrow in the direction of the highest q-value
+                    if best_direction == '↑':  # Draw an up arrow
+                        mid = (x1 + x2) / 2
+                        top = y1 + edge_dist_triangle
+                        triange_points = [mid, top, mid - triangle_width / 2, top + triangle_height, mid + triangle_width / 2, top + triangle_height]
+                        canvas.create_polygon(triange_points, fill='white')
+                    elif best_direction == '↓':  # Draw a down arrow
+                        mid = (x1 + x2) / 2
+                        top = y2 - edge_dist_triangle
+                        triange_points = [mid, top, mid - triangle_width / 2, top - triangle_height,
+                                          mid + triangle_width / 2, top - triangle_height]
+                        canvas.create_polygon(triange_points, fill='white')
+                    elif best_direction == '←':  # Draw a left arrow
+                        mid = (y1 + y2) / 2
+                        top = x1 + edge_dist_triangle
+                        triange_points = [top, mid, top + triangle_height, mid - triangle_width / 2,
+                                          top + triangle_height, mid + triangle_width / 2]
+                        canvas.create_polygon(triange_points, fill='white')
+                    elif best_direction == '→':  # Draw a right arrow
+                        mid = (y1 + y2) / 2
+                        top = x2 - edge_dist_triangle
+                        triange_points = [top, mid, top - triangle_height, mid - triangle_width / 2,
+                                          top - triangle_height, mid + triangle_width / 2]
+                        canvas.create_polygon(triange_points, fill='white')
+
+            else:  # This is a boulder state
+                x1 = edge_dist + col * ((canvas_width - 2 * edge_dist) / cols)
+                y1 = edge_dist + row * ((canvas_height - edge_dist - bottom_space) / rows)
+                x2 = x1 + ((canvas_width - 2 * edge_dist) / cols)
+                y2 = y1 + ((canvas_height - edge_dist - bottom_space) / rows)
+                canvas.create_rectangle(x1, y1, x2, y2, fill='grey', outline='white')
+
+    canvas.create_text(int(canvas_width / 2), canvas_height - bottom_space / 2, font=('TkDefaultFont', int(bottom_space / 2)),
+                       text=('VALUE AFTER ' + str(iterations) + ' ITERATIONS'), fill='white')  # Write text at the bottom of the canvas
+
+    canvas.pack()
+
+
+def get_best_move(state):
+    max_value = float('-inf')
+    max_index = -1
+
+    for move in range(len(state)):
+        if state[move][0] > max_value:
+            max_index = move
+            max_value = state[move][0]
+    return max_index
+main()
