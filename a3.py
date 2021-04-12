fd513109289e933b631dc840bec82b3033a07d7b
Jackson B <barkerj3@myumanitoba.ca>
2021-04-12 16:09:15 -0500

    slight cleanup
    -deleted commented out prints
    -added comments to some things
    -removed unnecessary input to ValueIterationAgent

--- a3.py
+++ a3.py
@@ -56,7 +56,6 @@ def parseLine(line):
   field = tokens[0]
 
 
-  #***SOME OF THESE X,Y ARE FLIPPED***
   if field == 'Horizontal':
     global HORIZONTAL
     HORIZONTAL = int(tokens[1])
@@ -114,22 +113,18 @@ def tests():
 
 class ValueIterationAgent:
 
-  def __init__(self,grid,terminal,boulder, startState, k, episodes, alpha, discount, noise, transitionCost):
+  def __init__(self,grid,terminal,boulder, k, discount, noise, transitionCost):
 
 
     self.grid = grid
     self.discount = discount
     self.k = k
-    self.alpha = alpha
-    self.episodes = episodes
     self.noise = noise
-    self.startState = startState
     self.transitionCost = transitionCost
     self.boulder = boulder
     self.terminal = terminal
-  
-  
-        
+
+    self.iterate()
 
   #of 1 state
   def getValue(self, state):
@@ -141,41 +136,22 @@ class ValueIterationAgent:
     else:
       values = []
       for val in self.grid[state[0]][state[1]]:
-        #print(f"val: {val} i: {state[0]} j: {state[1]}")
-        #print(f"val[0]: {val[0]}")
         values.append(val[0])
       return max(values)
-    
-
-  def getQValue(self, state, action):
-    pass
-
-  def getPolicy(self, state):
-    pass
-
-  def getAction(self, state):
-    pass
   
   def iterate(self):
     
-    rows = len(self.grid)  # Number of rows in the grid
+    rows = len(self.grid)
     cols = len(self.grid[0])
-
-    #print(f"(1,4) val: {self.transitionCost + self.discount*self.getValue((1,5))}")
-    #state = self.startState
-
-    
 
     for iteration in range(self.k):
       
+      #Copy the grid so we aren't using values that were updated in the same iteration
       self.newGrid = copy.deepcopy(self.grid)
       
       for i in range(rows):
         for j in range(cols):
-          
-          #vk = self.discount*self.getValue((i,j))
-          #print(f"vk: {vk}")
-          
+
           terminal_or_boulder = False
 
           for term in self.terminal:
@@ -189,66 +165,63 @@ class ValueIterationAgent:
           if not terminal_or_boulder:
             up = down = left = right = 0
 
-            valid = (i+1 < rows and i-1 >= 0 and j+1 < cols and j-1 >= 0)
-            mainProb = (1-self.noise)
-            noiseProb = self.noise/2
+            mainProb = (1-self.noise) #probability for desired direction
+            noiseProb = self.noise/2 #probability for each of the 2 'noise directions'
 
             upValid = i+1 < rows
             downValid = i-1 >= 0
             leftValid = j-1 >= 0
             rightValid = j+1 < cols
 
-            # [this][][][] is the x coord
-            # [][this][][] is the y coord
-            # []][][this][] is which of the 4 neighbouring cells it would go to (up,left,down,right)
-            # []][][][this] is the value (if it was [1] that would be the arrow since they look like: [0,'↑'])
+            
             if upValid: #up
-              #self.newGrid[i][j][0][0] = (mainProb*(self.transitionCost + (self.discount*self.getValue((i+1,j)))))
               up = (mainProb*(self.transitionCost + (self.discount*self.getValue((i+1,j)))))
               if rightValid:
                 up += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i,j+1)))))
               if leftValid:
                 up += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i,j-1)))))
+
             if leftValid:#left
-              #self.newGrid[i][j][1][0] = (mainProb*(self.transitionCost + (self.discount*self.getValue((i,j-1)))))
               left = (mainProb*(self.transitionCost + (self.discount*self.getValue((i,j-1)))))
               if upValid:
                 left += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i+1,j)))))
               if downValid:
                 left += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i-1,j)))))
+
             if downValid:#down
-              #self.newGrid[i][j][2][0] = (mainProb*(self.transitionCost + (self.discount*self.getValue((i-1,j)))))
               down = (mainProb*(self.transitionCost + (self.discount*self.getValue((i-1,j)))))
               if rightValid:
                 down += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i,j+1)))))
               if leftValid:
                 down += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i,j-1)))))
+
             if rightValid:#right
-              #self.newGrid[i][j][3][0] = (mainProb*(self.transitionCost + (self.discount*self.getValue((i,j+1)))))
               right = (mainProb*(self.transitionCost + (self.discount*self.getValue((i,j+1)))))
               if upValid:
                 right += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i+1,j)))))
               if downValid:
                 right += (noiseProb*(self.transitionCost + (self.discount*self.getValue((i-1,j)))))
 
+            # [this][][][] is the x coord
+            # [][this][][] is the y coord
+            # []][][this][] is which of the 4 neighbouring cells it would go to (up,left,down,right)
+            # []][][][this] is the value if [0] (if [1] that would be the arrow since they look like: [0,'↑'])
             self.newGrid[i][j][0][0] = up
             self.newGrid[i][j][1][0] = left
             self.newGrid[i][j][2][0] = down
             self.newGrid[i][j][3][0] = right
-          
+      
+      #update the grid with the new values for the next iteration
       self.grid = copy.deepcopy(self.newGrid)
     
 
 
 def main():
 
-    #(self,grid,terminal,boulder, startState, k, episodes, alpha, discount, noise, transitionCost)
-    
     window = tk.Tk()
     readInput('gridConf.txt')
     grid = createGrid(HORIZONTAL,VERTICAL)
-    valIter = ValueIterationAgent(grid,TERMINAL,BOULDER,ROBOTSTARTSTATE,K,EPISODES,ALPHA,DISCOUNT,NOISE,TRANSITION_COST)
-    valIter.iterate()
+    valIter = ValueIterationAgent(grid,TERMINAL,BOULDER,K,DISCOUNT,NOISE,TRANSITION_COST)
     grid = valIter.grid
     tests()
 
